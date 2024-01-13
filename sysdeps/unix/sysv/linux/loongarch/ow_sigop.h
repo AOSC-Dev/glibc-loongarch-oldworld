005314a4ff99dd67ba31a29d64595449d1d67f1a
Miao Wang <shankerwangmiao@gmail.com>
2024-01-14 05:15:07 +0800

    add sigaction & ucontext compat

--- sysdeps/unix/sysv/linux/loongarch/ow_sigop.h
+++ sysdeps/unix/sysv/linux/loongarch/ow_sigop.h
@@ -1,4 +1,5 @@
 #include <signal.h>
+#include <string.h>
 
 extern int __ow_sigaddset(sigset_t *, int);
 extern int __ow_sigdelset(sigset_t *, int);
@@ -7,10 +8,9 @@ extern int __ow_sigfillset(sigset_t *);
 extern int __ow_sigandset (sigset_t *__set, const sigset_t *__left, const sigset_t *__right);
 extern int __ow_sigorset (sigset_t *__set, const sigset_t *__left, const sigset_t *__right);
 
+#define __OW_NSIG 128
+
 static inline void __ow_clear_unused_bits(sigset_t *set)
 {
-    sigset_t tmp;
-    __ow_sigemptyset (&tmp);
-    sigfillset (&tmp);
-    __ow_sigandset(set, set, &tmp);
+    memset ((void *)set + (NSIG / 8), 0, (__OW_NSIG - NSIG) / 8);
 }
